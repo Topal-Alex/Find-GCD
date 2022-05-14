385090d7d0a69466f99703bd000e1fbd3ff458bb
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-05-14 15:36:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+
+
+namespace ConsoleApplication1
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gcd(43, 15));
+            
+
+        } 
+        public static int Gcd(int a, int b)
+        {
+            int ostatok;
+            int gdc = 0;
+            while(b != 0)
+            {
+                ostatok = a % b;
+                a = b;
+                b = ostatok;
+                gdc = a;
+               
+            }
+            return gdc;
+        }
+        
+    }
+}
